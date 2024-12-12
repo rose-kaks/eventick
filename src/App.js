a2c7ef77866244a3bc5df56862b458f6c7023592
rose-kaks <rosekakrania@gmail.com>
2024-12-12 12:39:38 +0530

    create a calendar for editable events

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Calendar from "./components/Calendar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
